e57b42bff35137da2d87428e30a8be2fbed39741
DanishSjjd <danishsjjad@gmail.com>
2023-02-11 10:00:31 +0500

    refactor(cards): manually adding id to crypto.randomUUID()

--- src/constants/card.tsx
+++ src/constants/card.tsx
@@ -1,11 +1,8 @@
 import besnik from "../assets/images/card-cover/besnik.jpg"
 import expice from "../assets/images/card-cover/expice.jpg"
 import fitClub from "../assets/images/card-cover/fitclub.jpg"
-// import gfox from "../assets/images/card-cover/gfox.jpg"
-// import hacks from "../assets/images/card-cover/hacks.jpg"
 import houseToHome from "../assets/images/card-cover/house-to-home.jpg"
 import wibeStudio from "../assets/images/card-cover/wibe-studio.png"
-import mcafee from "../assets/images/card-cover/mcafee.jpg"
 
 export type Card = {
   id: string
@@ -17,22 +14,6 @@ export type Card = {
 }
 
 export const cards: Card[] = [
-  // {
-  //   img: gfox,
-  //   title: "GFOX",
-  //   description:
-  //     "This landing page is for an NFT collection called GFOX. Made in React.js and has many interactive animations and sliders. Users of Android and Apple can conveniently use it on mobile, tablets, and desktops",
-  //   href: "https://gfox-danishsjjd.netlify.app/",
-  //   id: "a",
-  // },
-  // {
-  //   img: mcafee,
-  //   title: "Mcafee",
-  //   description:
-  //     "This Landing page is created in ReactJs and for NFT collection. Highly responsive, good user experience and nice interactive animations",
-  //   href: "https://mcafee-danishsjjd.netlify.app/",
-  //   id: "h",
-  // },
   {
     img: expice,
     title: "Expice",
@@ -47,23 +28,15 @@ export const cards: Card[] = [
     description:
       "This website is for fitness club. I implement CSS, animations, and slider to make user experience better so user spend there most of time. It works on both Android and iOS",
     href: "https://fitclub-danishsjjd.netlify.app/",
-    id: "c",
+    id: crypto.randomUUID(),
   },
-  // {
-  //   img: hacks,
-  //   title: "Hacks",
-  //   description:
-  //     "This Landing page is created in ReactJs and for NFT collection. Highly responsive, good user experience and nice interactive animations",
-  //   href: "https://hacks-danishsjjd.netlify.app/",
-  //   id: "b",
-  // },
   {
     img: wibeStudio,
     title: "Wibe Studio",
     description:
       "TODO: HAVE TO WRITE IT:- This website is for fitness club. I implement CSS, animations, and slider to make user experience better so user spend there most of time. It works on both Android and iOS",
     href: "https://wibe-studio-danishsjjd.netlify.app/",
-    id: "i",
+    id: crypto.randomUUID(),
   },
   {
     img: houseToHome,
@@ -72,7 +45,7 @@ export const cards: Card[] = [
       "This is a full-stack website I create using Reactjs, tailwindcss for the front-end, and for the backend, I use nodejs & expressJs for API, and for the payment method, I use stripe. This website also has an admin dashboard so the admin can add, delete or update products and also admin can remove users and reviews from products. It works on both Android and iOS. You can buy, review or track your product and much more",
     href: "https://house-to-home.herokuapp.com/",
     ifNotLiveMsg: "404 (Not Found)",
-    id: "f",
+    id: crypto.randomUUID(),
   },
   {
     img: besnik,
@@ -80,7 +53,6 @@ export const cards: Card[] = [
     description:
       "This website is developed in ReactJs, you can easily buy or rent a house. It works on both Android and iOS.",
     href: "https://besnik-danishsjjd.netlify.app/",
-    id: "g",
+    id: crypto.randomUUID(),
   },
 ]
-// latestId: i
