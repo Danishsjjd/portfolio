60d954d1b2b093f84a721a559b60c61f455c1f87
DanishSjjd <danishsjjad@gmail.com>
2023-12-17 16:27:07 +0500

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,21 @@ import { Toaster } from "sonner"
 
 export const metadata: Metadata = {
   title: "Danish Sajjad — Web Developer & Designer",
+  description:
+    "A Web Developer and Designer based in Pakistan with an eye for great design (UI) and even better user experience (UX).",
+  authors: [{ name: "Danish sajjad", url: "https://danishsjjd.dev" }],
+  keywords: [
+    "HTML",
+    " CSS",
+    " JavaScript",
+    " ReactJs",
+    " React js",
+    " react js",
+    " landing page",
+    " frontend",
+    " web design",
+  ],
+  metadataBase: new URL("https://danishsjjd.dev"),
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
