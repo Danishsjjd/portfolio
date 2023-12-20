b47c0878e741f7a5f9e576c7a57099eb9189b854
DanishSjjd <danishsjjad@gmail.com>
2023-12-20 17:51:56 +0500

    feat: icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#ffc40d" />
+      <meta name="theme-color" content="#ffffff" /> */}
       <body>
         <div id="root">{children}</div>
         <Toaster position="bottom-right" richColors />
